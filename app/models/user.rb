f20367a9fc6049287bfe0a877b4bf1c0cfbea29e
azu-M888 <machine27ft@gmail.com>
2024-06-22 20:57:23 +0900

    ユーザー管理機能2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
     VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-    validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'passwordは英字と数字の両方を含めて設定してください' }
+    validates :password, presence: true,format: { with: VALID_PASSWORD_REGEX, message: 'は英字と数字の両方を含めて設定してください' }
 
     VALID_ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥々]+\z/
-    validates :last_name, format: { with: VALID_ZENKAKU_REGEX, message: '姓と名は全角で入力してください。' }
-    validates :first_name, format: { with: VALID_ZENKAKU_REGEX, message: '姓と名は全角で入力してください。' }
-    validates :first_name_kana, format: { with: VALID_ZENKAKU_REGEX, message: 'カナ入力は全角で入力してください。' }
-    validates :last_name_kana, format: { with: VALID_ZENKAKU_REGEX, message: 'カナ入力は全角で入力してください。'  }
+    validates :last_name,presence: true, format: { with: VALID_ZENKAKU_REGEX, message: 'は全角で入力してください。' }
+    validates :first_name,presence: true,format: { with: VALID_ZENKAKU_REGEX, message: 'は全角で入力してください。' }
+    VALID_ZENKAKU_REGEX = /\A[ァ-ン]+\z/
+    validates :first_name_kana,presence: true, format: { with: VALID_ZENKAKU_REGEX, message: 'は全角カナで入力してください。' }
+    validates :last_name_kana,presence: true, format: { with: VALID_ZENKAKU_REGEX, message: 'は全角カナで入力してください。'  }
 
   validates :nickname, presence: true 
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :email, presence: true 
   validates :birth_date, presence: true
 
-  has_many :items, dependent: :destroy
-  has_many :orders,dependent: :destroy
+  # has_many :items, dependent: :destroy
+  # has_many :orders,dependent: :destroy
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,15 +5,16 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    
       it 'nicknameが空では登録できない' do
-        user = FactoryBot.build(:user)  # Userのインスタンス生成
-        user.nickname = ''  # nicknameの値を空にする
+        user = FactoryBot.build(:user) 
+        user.nickname = '' 
         user.valid?
-        expect(user.errors.full_messages).to include "Nickname can't be blank"
+        expect(user.errors.full_messages).to include ("Nickname can't be blank")
       end
       it 'emailが空では登録できない' do
-        user = FactoryBot.build(:user)  # Userのインスタンス生成
-        user.email = ''  # emailの値を空にする
+        user = FactoryBot.build(:user)  
+        user.email = '' 
         user.valid?
         expect(user.errors.full_messages).to include("Email can't be blank")
       end
@@ -29,22 +30,34 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Email is invalid')
       end
       it 'passwordが空では登録できない' do
-        user = FactoryBot.build(:user)  # Userのインスタンス生成
-        user.password = ''  # emailの値を空にする
+        user = FactoryBot.build(:user) 
+        user.password = '' 
         user.valid?
         expect(user.errors.full_messages).to include("Password can't be blank")
       end
-      it 'passwordはアルファベットと数字の混合である必要がある' do
-        user = FactoryBot.build(:user)
-        user.password = '123456' # 変数名を @user から user に修正
-        user.valid?
-        expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
-      end
       it 'passwordが5文字以下では登録できない' do
-        @user.password = '1234' # パスワードを5文字以下に設定
+        @user.password = '12345' 
         @user.password_confirmation = @user.password
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)") # 期待するエラーメッセージを修正
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)") 
+      end
+      it 'passwordは英字のみでは登録できない' do
+        user = FactoryBot.build(:user)
+        user.password = 'ABCDEF'
+        user.valid?
+        expect(user.errors.full_messages).to include("Password は英字と数字の両方を含めて設定してください")
+      end
+      it 'passwordは数字のみでは登録できない' do
+        user = FactoryBot.build(:user)
+        user.password = '123456'
+        user.valid?
+        expect(user.errors.full_messages).to include("Password は英字と数字の両方を含めて設定してください")
+      end
+      it 'passwordは全角文字を含むと登録できない' do
+        user = FactoryBot.build(:user)
+        user.password = '12国国AB'
+        user.valid?
+        expect(user.errors.full_messages).to include("Password は英字と数字の両方を含めて設定してください")
       end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = '123456'
@@ -52,38 +65,38 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      it "first_nameとlast_nameが空では登録できない" do
-        user = FactoryBot.build(:user, first_name: nil, last_name: nil)
+      it "first_name、last_name、first_name_kana、last_name_kanaが空では登録できない" do
+        user = FactoryBot.build(:user)
+        user.first_name = ''
+        user.last_name = ''
+        user.first_name_kana = ''
+        user.last_name_kana = ''
         user.valid?
-        expect(user.errors.full_messages).to include "First name can't be blank"
-        expect(user.errors.full_messages).to include "Last name can't be blank"
+        expect(user.errors.full_messages).to include ("First name can't be blank")
+        expect(user.errors.full_messages).to include ("Last name can't be blank")
+        expect(user.errors.full_messages).to include ("First name kana can't be blank")
+        expect(user.errors.full_messages).to include ("Last name kana can't be blank")
       end
-      it "first_nameとlast_nameは全角以外では登録できない" do
-        user = FactoryBot.build(:user, first_name: nil, last_name: nil)
-        user.first_name = '123456'
-        user.last_name = '123456'
+      it "first_name、last_nameは全角以外では登録できない" do
+        user = FactoryBot.build(:user)
+        user.first_name_kana = '121356'
+        user.last_name_kana = '121356'
         user.valid?
-        expect(user.errors.full_messages).to include "First name 姓と名は全角で入力してください。"
-        expect(user.errors.full_messages).to include "Last name 姓と名は全角で入力してください。"
+        expect(user.errors.full_messages).to include ("First name kana は全角で入力してください。")
+        expect(user.errors.full_messages).to include ("Last name kana は全角で入力してください。")
       end
-      it "first_name_kanaとlast_name_kanaが空では登録できない" do
-        user = FactoryBot.build(:user, first_name_kana: nil, last_name_kana: nil)
+      it "first_name_kana、last_name_kanaは全角カナ以外では登録できない" do
+        user = FactoryBot.build(:user)
+        user.first_name_kana = '12国国56'
+        user.last_name_kana = '12国国56'
         user.valid?
-        expect(user.errors.full_messages).to include "First name kana can't be blank"
-        expect(user.errors.full_messages).to include "Last name kana can't be blank"
-      end
-      it "first_name_kanaとlast_name_kanaは全角以外では登録できない" do
-        user = FactoryBot.build(:user, first_name: nil, last_name: nil)
-        user.first_name_kana = '123456'
-        user.last_name_kana = '123456'
-        user.valid?
-        expect(user.errors.full_messages).to include "First name kana カナ入力は全角で入力してください。"
-        expect(user.errors.full_messages).to include "Last name kana カナ入力は全角で入力してください。"
+        expect(user.errors.full_messages).to include ("First name kana は全角カナで入力してください。")
+        expect(user.errors.full_messages).to include ("Last name kana は全角カナで入力してください。")
       end
       it "birth_dateが空では登録できない" do
         user = FactoryBot.build(:user, birth_date: nil)
         user.valid?
-        expect(user.errors.full_messages).to include "Birth date can't be blank"
+        expect(user.errors.full_messages).to include ("Birth date can't be blank")
       end
   end
 end
