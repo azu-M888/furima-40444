4539fe318fc2a06a0c32a5963150e87643ca07e1
azu-M888 <machine27ft@gmail.com>
2024-06-22 22:51:53 +0900

    ユーザー管理機能2-5

--- db/schema.rb
+++ db/schema.rb
@@ -10,44 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_16_053150) do
-  create_table "addresses", charset: "utf8", force: :cascade do |t|
-    t.bigint "order_id", null: false
-    t.string "postal_code", null: false
-    t.integer "prefecture_id", null: false
-    t.string "city", null: false
-    t.string "address", null: false
-    t.string "biilding", null: false
-    t.string "phone_number", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_addresses_on_order_id"
-  end
-
-  create_table "items", charset: "utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "item_name", null: false
-    t.text "item_info", null: false
-    t.integer "item_category_id", null: false
-    t.integer "item_sales_status_id", null: false
-    t.integer "item_shipping_fee_status_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "item_scheduled_delivery_id", null: false
-    t.integer "item_price", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "orders", charset: "utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "item_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_orders_on_item_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2024_06_16_052837) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "last_name", null: false
@@ -66,8 +29,4 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_16_053150) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "addresses", "orders"
-  add_foreign_key "items", "users"
-  add_foreign_key "orders", "items"
-  add_foreign_key "orders", "users"
 end
