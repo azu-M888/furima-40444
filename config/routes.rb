e10cccd8af88c26b77d31d50871c201fff0015eb
azu-M888 <machine27ft@gmail.com>
2024-07-06 15:29:20 +0900

    商品削除機能1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:show,:index,:new,:edit,:update,:create]
+  resources :items
   # resources :orders do
   #  resources :addresses,only: [:create]
   # end
