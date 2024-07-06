78f97ffb32305a985edbc6f4499e28b55954d699
azu-M888 <machine27ft@gmail.com>
2024-07-06 16:09:09 +0900

    商品実装機能3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -62,8 +62,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    else
     @item.destroy
     redirect_to root_path
+    end
   end
 
 
